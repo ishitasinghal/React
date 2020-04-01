0a4349d1ec194dc7da0c07f1ba940abd0965d22d
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-01 17:26:05 +0530

    Update index2.js

--- react1/index2.js
+++ react1/index2.js
@@ -1,12 +1,11 @@
-// Objective: Fill in the boilerplate React code required to render an
-// unordered list (<ul>) to the page. The list should contain 3 list items
-// (<li>) with anything in them you want.
-
-// HINTS:
-// import the libraries you need first
-// use one of the libraries to render some JSX to the page 
-
 import React from "react"
-import Reactdom from "react-dom"
+import ReactDOM from "react-dom"
 
-Reactdom.render(<ul><l1>item1</l1><l2>item2</l2><l3>item3</l3></ul>,document.getElementById("root"))
+ReactDOM.render(
+  <ul>
+    <li>item1</li>
+    <li>item2</li>
+    <li>item3</li>
+  </ul>,
+  document.getElementById("root")
+)
