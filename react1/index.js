0535706af671d02208ae31eab05d58691f42fcd4
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-01 17:11:29 +0530

    Update index.js

--- react1/index.js
+++ react1/index.js
@@ -1,4 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<div><h1>Reaction from react</h1><p1>Happy coding</p1></div>, document.getElementById("root"))
+// JSX
+ReactDOM.render(<div><h1>Reaction form react</h1><p>Happy coding</p></div>, document.getElementById("root"))
+
+var myNewP = document.createElement("p")
+myNewP.innerHTML = "Happy Coding!"
