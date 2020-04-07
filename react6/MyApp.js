2b300e4c191b1afd3cba61bfb8f336406f45fac0
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-07 22:58:00 +0530

    Add files via upload

---
+++ react6/MyApp.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function MyApp(){
+    return(
+        <div>
+            <input type = "checkbox" />
+            <p> This is checkbox 1 </p>
+            
+            <input type = "checkbox" />
+            <p> This is checkbox 2 </p>
+            
+            <input type = "checkbox" />
+            <p> This is checkbox 3 </p>
+            
+            <input type = "checkbox" />
+            <p> This is checkbox 4 </p>
+            
+        </div>
+    )
+}
+
+export default MyApp
---
+++ react6/index.js
@@ -0,0 +1,12 @@
+// From scratch, initialize the React app
+// Render an <App /> component
+// Create the <App /> component from scratch
+// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
+// like you're making a todo list with some hard-coded items on it
+
+import React from "react"
+import ReactDom from "react-dom"
+
+import MyApp from "./MyApp.js"
+
+ReactDom.render(<MyApp />, document.getElementById("root"))
