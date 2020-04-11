81eabf1f50be803a6be2a50c311d82f0dd6ce94c
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-11 17:55:24 +0530

    Update wisher2.js

--- react7/timewisher/wisher2.js
+++ react7/timewisher/wisher2.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 function Time() {
-  const date = new Date(2020, 4, 11, 17)
+  const date = new Date(2020, 4, 11, 10)
   const hours = date.getHours()
   const styles = {
     fontsize: "20"
@@ -12,7 +12,8 @@ function Time() {
   if (hours < 12) {
     time = "Morning"
     styles.color = "#00FFFF"
-  } 
+    styles.backgroundImage = url("https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fsun-rise&psig=AOvVaw0vGAfekX_uWB-nvgNVRPDW&ust=1586694274284000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCODPwfeu4OgCFQAAAAAdAAAAABAD")
+    } 
   else if (hours >= 12 && hours < 18) {
     time = "Afternoon"
     styles.color = "#FFA500"
