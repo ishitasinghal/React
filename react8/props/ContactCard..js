3915a42177d418e87bce8929745e0323bfa90aa6
Ishita Singhal <ishitasinghal08@gmail.com>
2020-04-19 22:49:58 +0530

    adding props.

---
+++ react8/props/ContactCard..js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function ContactCard(props) {
+    return (
+        <div className="contact-card">
+            <img src={props.imgUrl}/>
+            <h3>{props.name}</h3>
+            <p>Phone: {props.phone}</p>
+            <p>Email: {props.email}</p>
+        </div>
+    )
+}
+
+export default ContactCard
